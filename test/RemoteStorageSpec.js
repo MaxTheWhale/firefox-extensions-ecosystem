870bc09bdcdc3ad38f0cda36d31d51d03e4e3d17
Yang Li <muxuanxiang@163.com>
2019-02-11 03:27:54 +0000

    Changed to async/await in Spec

--- test/RemoteStorageSpec.js
+++ test/RemoteStorageSpec.js
@@ -63,84 +63,152 @@ describe("Google Drive", function() {
         // });
     }, timeOut);
 
-    // it("Should be able to delete a file without error", (done) => {
-    //     remoteStore.uploadFile("This is a delete test", "deleteTest.txt").then(() => {
-    //         remoteStore.deleteFile("deleteTest.txt").then(() => {
-    //             remoteStore.getInfo().then((result) => {
-    //                 expect(result["deleteTest.txt"]).not.toBeDefined();
-    //                 done();
-    //             });
-    //         }).catch((e) => {
-    //             fail(e);
-    //         });
-    //     });
-    // }, timeOut);
+    it("Should be able to delete a file without error", (done) => {
+        try {
+            await remoteStore.uploadFile("This is a delete test", "deleteTest.txt");
+            result = await remoteStore.getInfo();
+        } catch(e) {
+            error = e;
+        }
+        expect(result["deleteTest.txt"]).not.toBeDefined();
+        expect(error).not.toBeDefined();
+        done();
+        
+        // remoteStore.uploadFile("This is a delete test", "deleteTest.txt").then(() => {
+        //     remoteStore.deleteFile("deleteTest.txt").then(() => {
+        //         remoteStore.getInfo().then((result) => {
+        //             expect(result["deleteTest.txt"]).not.toBeDefined();
+        //             done();
+        //         });
+        //     }).catch((e) => {
+        //         fail(e);
+        //     });
+        // });
+    }, timeOut);
 
-    // it("Should be able to get correct file's info", (done) => {
-    //     remoteStore.uploadFile("This is getInfo test1", "infoTest1.txt").then(() => {
-    //         remoteStore.getInfo("infoTest1.txt").then((result) => {
-    //             expect(result.name).toMatch("infoTest1.txt");
-    //             expect(result.mimeType).toMatch("text/plain");
-    //             done();
-    //         }).catch((e) => {
-    //             fail(e);
-    //         });
-    //     });
-    // }, timeOut);
+    it("Should be able to get correct file's info", (done) => {
+        try {
+            await remoteStore.uploadFile("This is getInfo test1", "infoTest1.txt");
+            result = await remoteStore.getInfo("infoTest1.txt");
+        } catch(e) {
+            error = e;
+        }
+        expect(result.name).toMatch("infoTest1.txt");
+        expect(result.mimeType).toMatch("text/plain");
+        expect(error).not.toBeDefined();
+        done();
 
-    // it ("Should be able to get all files info", (done) => {
-    //     remoteStore.uploadFile("This is getInfo test2", "infoTest2.txt").then(() => {
-    //         remoteStore.getInfo().then((result) => {
-    //             expect(result["infoTest2.txt"]).toBeDefined();
-    //             expect(result["infoTest2.txt"].name).toMatch("infoTest2.txt");
-    //             expect(result["infoTest2.txt"].mimeType).toMatch("text/plain");
-    //             done();
-    //         }).catch((e) => {
-    //             fail(e);
-    //         });
-    //     }); 
-    // }, timeOut);
+        // remoteStore.uploadFile("This is getInfo test1", "infoTest1.txt").then(() => {
+        //     remoteStore.getInfo("infoTest1.txt").then((result) => {
+        //         expect(result.name).toMatch("infoTest1.txt");
+        //         expect(result.mimeType).toMatch("text/plain");
+        //         done();
+        //     }).catch((e) => {
+        //         fail(e);
+        //     });
+        // });
+    }, timeOut);
 
-    // it("Should be able to overwrite a file", (done) => {
-    //     remoteStore.uploadFile("This text should get overwritten", "overwriteTest.txt").then(() => {
-    //         remoteStore.uploadFile("This is an overwrite test", "overwriteTest.txt").then(() => {
-    //             remoteStore.downloadFile("overwriteTest.txt").then((result) => {
-    //                 new Response(result).text().then((text) => {
-    //                     expect(text).toMatch("This is an overwrite test");
-    //                     done();
-    //                 })
-    //             });
-    //         });
-    //     });
-    // }, timeOut);
+    it ("Should be able to get all files info", (done) => {
+        try {
+            await remoteStore.uploadFile("This is getInfo test2", "infoTest2.txt");
+            result = await remoteStore.getInfo();
+        } catch(e) {
+            error = e;
+        }
+        expect(result["infoTest2.txt"]).toBeDefined();
+        expect(result["infoTest2.txt"].name).toMatch("infoTest2.txt");
+        expect(result["infoTest2.txt"].mimeType).toMatch("text/plain");
+        expect(error).not.toBeDefined();
+        done();
 
-    // it("Should be able to upload a large file", (done) => {
-    //     fetch("large_file.png").then((file) => {
-    //         file.blob().then((fileBlob) => {
-    //             remoteStore.uploadFile(fileBlob, "largeUploadTest.png").then((result) => {
-    //                 expect(result / 100).toEqual(2);
-    //                 done();
-    //             }).catch((e) => {
-    //                 fail(e);
-    //             });
-    //         });
-    //     });
-    // }, largeTimeOut);
+        // remoteStore.uploadFile("This is getInfo test2", "infoTest2.txt").then(() => {
+        //     remoteStore.getInfo().then((result) => {
+        //         expect(result["infoTest2.txt"]).toBeDefined();
+        //         expect(result["infoTest2.txt"].name).toMatch("infoTest2.txt");
+        //         expect(result["infoTest2.txt"].mimeType).toMatch("text/plain");
+        //         done();
+        //     }).catch((e) => {
+        //         fail(e);
+        //     });
+        // }); 
+    }, timeOut);
 
-    // it("Should be able to download a large file", (done) => {
-    //     fetch("large_file.png").then((file) => {
-    //         file.blob().then((fileBlob) => {
-    //             remoteStore.uploadFile(fileBlob, "largeDownloadTest.png").then(() => {
-    //                 remoteStore.downloadFile("largeDownloadTest.png").then((result) => { 
-    //                     expect(result.size).toEqual(fileBlob.size);
-    //                     done();
-    //                 }).catch((e) => {
-    //                     fail(e);
-    //                 });
-    //             });
-    //         });
-    //     });
-    // }, largeTimeOut);
+    it("Should be able to overwrite a file", (done) => {
+        try {
+            await remoteStore.uploadFile("This text should get overwritten", "overwriteTest.txt");
+            await remoteStore.uploadFile("This is an overwrite test", "overwriteTest.txt");
+            result = await remoteStore.downloadFile("overwriteTest.txt")
+            text = await new Response(result).text();
+        } catch(e) {
+            error = e;
+        }
+        expect(text).toMatch("This is an overwrite test");
+        expect(error).not.toBeDefined();
+        done();
+
+        // remoteStore.uploadFile("This text should get overwritten", "overwriteTest.txt").then(() => {
+        //     remoteStore.uploadFile("This is an overwrite test", "overwriteTest.txt").then(() => {
+        //         remoteStore.downloadFile("overwriteTest.txt").then((result) => {
+        //             new Response(result).text().then((text) => {
+        //                 expect(text).toMatch("This is an overwrite test");
+        //                 done();
+        //             })
+        //         });
+        //     });
+        // });
+    }, timeOut);
+
+    it("Should be able to upload a large file", (done) => {
+        try {
+            file = await fetch("large_file.png");
+            fileBlob = await file.blob();
+            result = await remoteStore.uploadFile(fileBlob, "largeUploadTest.png");
+        } catch(e) {
+            error = e;
+        }
+        expect(result / 100).toEqual(2);
+        expect(error).not.toBeDefined();
+        done();
+
+        // fetch("large_file.png").then((file) => {
+        //     file.blob().then((fileBlob) => {
+        //         remoteStore.uploadFile(fileBlob, "largeUploadTest.png").then((result) => {
+        //             expect(result / 100).toEqual(2);
+        //             done();
+        //         }).catch((e) => {
+        //             fail(e);
+        //         });
+        //     });
+        // });
+    }, largeTimeOut);
+
+    it("Should be able to download a large file", (done) => {
+        try {
+            file = await fetch("large_file.png");
+            fileBlob = await file.blob();
+            await remoteStore.uploadFile(fileBlob, "largeDownloadTest.png");
+            result = await remoteStore.downloadFile("largeDownloadTest.png");
+        } catch(e) {
+            error = e;
+        }
+        expect(result.size).toEqual(fileBlob.size);
+        expect(error).not.toBeDefined();
+        done();
+
+        // fetch("large_file.png").then((file) => {
+        //     file.blob().then((fileBlob) => {
+        //         remoteStore.uploadFile(fileBlob, "largeDownloadTest.png").then(() => {
+        //             remoteStore.downloadFile("largeDownloadTest.png").then((result) => { 
+        //                 expect(result.size).toEqual(fileBlob.size);
+        //                 done();
+        //             }).catch((e) => {
+        //                 fail(e);
+        //             });
+        //         });
+        //     });
+        // });
+    }, largeTimeOut);
 
 });
 
