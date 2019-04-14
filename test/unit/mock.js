fef6b07b8f66e81101fb90c32481a5387c2e4907
Max Whale <maxwhale99@gmail.com>
2019-04-14 19:33:23 +0100

    Fix browser export

--- test/unit/mock.js
+++ test/unit/mock.js
@@ -3,21 +3,22 @@ const TEST_URL = "https://remotestoretestredirect.url/";
 const AUTH_ONEDRIVE = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
 const AUTH_GOOGLE = "https://accounts.google.com/o/oauth2/auth";
 
-let identity = {
-    getRedirectURL: function() { return TEST_URL; },
-    launchWebAuthFlow: async function(opts) {
-        if (opts.url.startsWith(AUTH_ONEDRIVE)) {
-            return `${opts.url}&access_token=${TEST_TOKEN}`;
+let browser = {
+    identity: {
+        getRedirectURL: function() { return TEST_URL; },
+        launchWebAuthFlow: async function(opts) {
+            if (opts.url.startsWith(AUTH_ONEDRIVE)) {
+                return `${opts.url}&access_token=${TEST_TOKEN}`;
+            }
+            if (opts.url.startsWith(AUTH_GOOGLE)) {
+                return `${opts.url}&access_token=${TEST_TOKEN}`;
+            }
+            return "none_matched";
         }
-        if (opts.url.startsWith(AUTH_GOOGLE)) {
-            return `${opts.url}&access_token=${TEST_TOKEN}`;
-        }
-        return "none_matched";
+    },
+    runtime: {
+        id: "{1234-extensionid}"
     }
 };
 
-let runtime = {
-    id: "{1234-extensionid}"
-};
-
-export {identity, runtime};
+export default browser;
