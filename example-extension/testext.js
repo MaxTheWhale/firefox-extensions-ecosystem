89013d5088c7603d1c04e02b7a1adb39951fb84e
MaxTheWhale <maxwhale99@gmail.com>
2019-04-25 20:55:34 +0100

    Add OneDrive to example extension

--- example-extension/testext.js
+++ example-extension/testext.js
@@ -2,28 +2,32 @@ import createRemoteStorage from './remoteStorage.js';
 
 const secret = "qOtT31KwyCmjRsHleg3tRR5_";
 const googleClientID = "330942350415-mc2p3pb057ckobr3smh1tekfrck9e7gr.apps.googleusercontent.com";
+const onedriveClientID = "6d8bb06d-0a42-4777-bb22-7efc0fedc8f9";
 
-async function useGoogle() {
-    let googleDrive = await createRemoteStorage("google", googleClientID); //Creating new storage.remote folder in drive
+async function testStorage(provider, clientID) {
+    let remoteStore = await createRemoteStorage(provider, clientID); //Creating new storage.remote folder in drive
     let fold1 = "Folder1";
     let fold2 = "Folder2";
-    await googleDrive.createFolder(fold1);
-    await googleDrive.createFolder(fold2);
-    let result = await googleDrive.getItems(true);
-    await googleDrive.uploadFile("Test file", "test", result[fold1].id);
-    await googleDrive.uploadFile("Test file", "test", result[fold2].id);
-    await googleDrive.uploadFile("Test file1", "test", result[fold1].id);
-    let resp = await googleDrive.downloadFile("test", result[fold1].id);
+    await remoteStore.createFolder(fold1);
+    await remoteStore.createFolder(fold2);
+    let result = await remoteStore.getItems(true);
+    await remoteStore.uploadFile("Test file", "test", result[fold1].id);
+    await remoteStore.uploadFile("Test file", "test", result[fold2].id);
+    await remoteStore.uploadFile("Test file1", "test", result[fold1].id);
+    let resp = await remoteStore.downloadFile("test", result[fold1].id);
     let text = await new Response(resp).text();
     console.log(text);
-    resp = await googleDrive.downloadFile("test", result[fold2].id);
+    resp = await remoteStore.downloadFile("test", result[fold2].id);
     text = await new Response(resp).text();
     console.log(text);
-    await googleDrive.deleteFile("test", result[fold1].id);
-    await googleDrive.deleteFile("test", result[fold2].id);
-    await googleDrive.deleteFile(fold1);
-    await googleDrive.deleteFile(fold2);
+    await remoteStore.deleteFile("test", result[fold1].id);
+    await remoteStore.deleteFile("test", result[fold2].id);
+    await remoteStore.deleteFile(fold1);
+    await remoteStore.deleteFile(fold2);
 }
 
 let googleButton = document.getElementById("google_button");
-googleButton.addEventListener("click", useGoogle);
+googleButton.addEventListener("click", () => {testStorage("google", googleClientID)});
+
+let onedriveButton = document.getElementById("onedrive_button");
+onedriveButton.addEventListener("click", () => {testStorage("onedrive", onedriveClientID)});
